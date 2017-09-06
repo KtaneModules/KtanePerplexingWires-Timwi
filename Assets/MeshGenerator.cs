ed050144183c2479b0691e514a2bd023c0ce3ea8
Timwi <github@timwi.de>
2017-09-06 05:31:38 +0200

    Generate wires, generate solution

---
+++ Assets/MeshGenerator.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+using Rnd = System.Random;
+
+namespace PerplexingWires
+{
+    public class MeshGenerator
+    {
+        public const double _wireRadius = .0025;
+        public const double _wireRadiusHighlight = .005;
+
+        const double _wireMaxSegmentDeviation = .001;
+        const double _wireMinBézierDeviation = .005;
+        const double _wireMaxBézierDeviation = .01;
+
+        const double _bottom = -.02;
+        const double _firstControlHeight = .01;
+        const double _interpolateHeight = .005;
+        const double _firstControlHeightHighlight = .005;
+        const double _interpolateHeightHighlight = .003;
+
+        sealed class CPC { public Pt ControlBefore, Point, ControlAfter; }
+
+        public enum WirePiece { Uncut, Cut, Copper }
+
+        public static Mesh GenerateWire(Pt start, Pt startControl, Pt endControl, Pt end, int numSegments, WirePiece piece, bool highlight, int seed, double raiseFactor)
+        {
+            const int bézierSteps = 16;
+            const int tubeRevSteps = 16;
+
+            var rnd = new Rnd(seed);
+            var thickness = highlight ? _wireRadiusHighlight : _wireRadius;
+
+            var raise = ((startControl - start) + (endControl - end)) * raiseFactor;
+            var interpolatedPoints = Ut.NewArray<Pt>(numSegments - 1, i => raise + startControl + (endControl - startControl) * (i + 1) / numSegments);
+            var controlPointsB = Ut.NewArray<Pt>(numSegments - 1, i =>
+            {
+                var p1 = interpolatedPoints[i];
+                var p2 = i == numSegments - 2 ? endControl : interpolatedPoints[i + 1];
+                var v = (p2 - p1).Normalize() * p1.Distance(p2) * .4;
+                var dummy = v.X > .5 ? new Pt(0, 1, 0) : new Pt(1, 0, 0);
+                var perpendicular = dummy * v;
+                v = (p1 + v).Rotate(p1, p1 + perpendicular, 45 * rnd.NextDouble());
+                v = v.Rotate(p1, p2, 360 * rnd.NextDouble());
+                return v;
+            });
+            var controlPointsA = Ut.NewArray<Pt>(numSegments - 1, i => 2 * interpolatedPoints[i] - controlPointsB[i]);
+
+            if (piece == WirePiece.Uncut)
+            {
+                var points =
+                    new[] { new { ControlBefore = default(Pt), Point = start, ControlAfter = startControl } }
+                    .Concat(interpolatedPoints.Select((p, i) => new { ControlBefore = controlPointsA[i], Point = p, ControlAfter = controlPointsB[i] }))
+                    .Concat(new[] { new { ControlBefore = endControl, Point = end, ControlAfter = default(Pt) } })
+                    .SelectConsecutivePairs(false, (one, two) => bézier(one.Point, one.ControlAfter, two.ControlBefore, two.Point, bézierSteps))
+                    .SelectMany((x, i) => i == 0 ? x : x.Skip(1))
+                    .ToArray();
+                return toMesh(createFaces(false, true, tubeFromCurve(points, thickness, tubeRevSteps)));
+                //return toMesh(createFaces(false, true, tubeFromCurve(new[] { start, startControl }.Concat(interpolatedPoints.SelectMany((p, ix) => new[] { controlPointsA[ix], p, controlPointsB[ix] })).Concat(new[] { endControl, end }).ToArray(), thickness, tubeRevSteps)));
+            }
+
+            var partialWire = new Func<IEnumerable<CPC>, IEnumerable<VertexInfo[]>>(pts =>
+            {
+                var points = pts
+                    .SelectConsecutivePairs(false, (one, two) => bézier(one.Point, one.ControlAfter, two.ControlBefore, two.Point, bézierSteps))
+                    .SelectMany((x, i) => i == 0 ? x : x.Skip(1))
+                    .ToArray();
+
+                var reserveForCopper = 6;
+                var discardCopper = 2;
+
+                if (piece == WirePiece.Cut)
+                {
+                    var tube = tubeFromCurve(points, thickness, tubeRevSteps).SkipLast(reserveForCopper).ToArray();
+                    var capCenter = points[points.Length - 1 - reserveForCopper];
+                    var normal = capCenter - points[points.Length - 2 - reserveForCopper];
+                    var cap = tube[tube.Length - 1].SelectConsecutivePairs(true, (v1, v2) => new[] { capCenter, v2.Point, v1.Point }.Select(p => new VertexInfo { Point = p, Normal = normal }).ToArray()).ToArray();
+                    return createFaces(false, true, tube).Concat(cap);
+                }
+                else
+                {
+                    var copper = tubeFromCurve(points.TakeLast(reserveForCopper + 2).SkipLast(discardCopper).ToArray(), thickness / 2, tubeRevSteps).Skip(1).ToArray();
+                    var copperCapCenter = points[points.Length - 1 - discardCopper];
+                    var copperNormal = copperCapCenter - points[points.Length - 2];
+                    var copperCap = copper[copper.Length - 1].SelectConsecutivePairs(true, (v1, v2) => new[] { copperCapCenter, v2.Point, v1.Point }.Select(p => new VertexInfo { Point = p, Normal = copperNormal }).ToArray()).ToArray();
+                    return createFaces(false, true, copper).Concat(copperCap);
+                }
+            });
+
+            var cutOffEarly = rnd.Next(2) == 0;
+            var angleForward = rnd.Next(2) == 0;
+            var rotAngle = (rnd.NextDouble() * 20 + 10) * (angleForward ? -1 : 1);
+            var rotAxisStart = start;
+            var rotAxisEnd = startControl;
+            Func<Pt, Pt> rot = p => p.Rotate(rotAxisStart, rotAxisEnd, rotAngle);
+            var beforeCut =
+                new[] { new CPC { ControlBefore = default(Pt), Point = start, ControlAfter = startControl } }
+                .Concat(interpolatedPoints.Take((cutOffEarly ? numSegments : numSegments + 1) / 2).Select((p, i) => new CPC { ControlBefore = rot(controlPointsA[i]), Point = rot(p), ControlAfter = rot(controlPointsB[i]) }));
+            var bcTube = partialWire(beforeCut);
+
+            var cutOffPoint = (cutOffEarly ? numSegments - 2 : numSegments - 1) / 2;
+            rotAngle = (rnd.NextDouble() * 20 + 10) * (angleForward ? -1 : 1);
+            rotAxisStart = end;
+            rotAxisEnd = endControl;
+            var afterCut =
+                new[] { new CPC { ControlBefore = default(Pt), Point = end, ControlAfter = endControl } }
+                .Concat(interpolatedPoints.Skip(cutOffPoint).Select((p, i) => new CPC { ControlBefore = rot(controlPointsB[i + cutOffPoint]), Point = rot(p), ControlAfter = rot(controlPointsA[i + cutOffPoint]) }).Reverse());
+            var acTube = partialWire(afterCut);
+
+            return toMesh(bcTube.Concat(acTube).ToArray());
+        }
+
+        sealed class VertexInfo
+        {
+            public Pt Point;
+            public Pt Normal;
+            public Vector3 V { get { return new Vector3((float) Point.X, (float) Point.Y, (float) Point.Z); } }
+            public Vector3 N { get { return new Vector3((float) Normal.X, (float) Normal.Y, (float) Normal.Z); } }
+        }
+
+        private static Mesh toMesh(VertexInfo[][] triangles)
+        {
+            return new Mesh
+            {
+                vertices = triangles.SelectMany(t => t).Select(v => v.V).ToArray(),
+                normals = triangles.SelectMany(t => t).Select(v => v.N).ToArray(),
+                triangles = triangles.SelectMany(t => t).Select((v, i) => i).ToArray()
+            };
+        }
+
+        // Converts a 2D array of vertices into triangles by joining each vertex with the next in each dimension
+        private static VertexInfo[][] createFaces(bool closedX, bool closedY, VertexInfo[][] meshData)
+        {
+            var len = meshData[0].Length;
+            return Enumerable.Range(0, meshData.Length).SelectManyConsecutivePairs(closedX, (i1, i2) =>
+                Enumerable.Range(0, len).SelectManyConsecutivePairs(closedY, (j1, j2) => new[]
+                {
+                    // triangle 1
+                    new[] { meshData[i1][j1], meshData[i2][j1], meshData[i2][j2] },
+                    // triangle 2
+                    new[] { meshData[i1][j1], meshData[i2][j2], meshData[i1][j2] }
+                }))
+                    .ToArray();
+        }
+
+        private static VertexInfo[][] tubeFromCurve(Pt[] pts, double radius, int revSteps)
+        {
+            var normals = new Pt[pts.Length];
+            normals[0] = ((pts[1] - pts[0]) * pt(0, 1, 0)).Normalize() * radius;
+            for (int i = 1; i < pts.Length - 1; i++)
+                normals[i] = normals[i - 1].ProjectOntoPlane((pts[i + 1] - pts[i]) + (pts[i] - pts[i - 1])).Normalize() * radius;
+            normals[pts.Length - 1] = normals[pts.Length - 2].ProjectOntoPlane(pts[pts.Length - 1] - pts[pts.Length - 2]).Normalize() * radius;
+
+            var axes = pts.Select((p, i) =>
+                i == 0 ? new { Start = pts[0], End = pts[1] } :
+                i == pts.Length - 1 ? new { Start = pts[pts.Length - 2], End = pts[pts.Length - 1] } :
+                new { Start = p, End = p + (pts[i + 1] - p) + (p - pts[i - 1]) }).ToArray();
+
+            return Enumerable.Range(0, pts.Length)
+                .Select(ix => new { Axis = axes[ix], Perp = pts[ix] + normals[ix], Point = pts[ix] })
+                .Select(inf => Enumerable.Range(0, revSteps)
+                    .Select(i => 360 * i / revSteps)
+                    .Select(angle => inf.Perp.Rotate(inf.Axis.Start, inf.Axis.End, angle))
+                    .Select(p => new VertexInfo { Point = p, Normal = p - inf.Point }).Reverse().ToArray())
+                .ToArray();
+        }
+
+        private static IEnumerable<Pt> bézier(Pt start, Pt control1, Pt control2, Pt end, int steps)
+        {
+            return Enumerable.Range(0, steps)
+                .Select(i => (double) i / (steps - 1))
+                .Select(t => pow(1 - t, 3) * start + 3 * pow(1 - t, 2) * t * control1 + 3 * (1 - t) * t * t * control2 + pow(t, 3) * end);
+        }
+
+        static double sin(double x)
+        {
+            return Math.Sin(x * Math.PI / 180);
+        }
+
+        static double cos(double x)
+        {
+            return Math.Cos(x * Math.PI / 180);
+        }
+
+        static double pow(double x, double y)
+        {
+            return Math.Pow(x, y);
+        }
+
+        static Pt pt(double x, double y, double z)
+        {
+            return new Pt(x, y, z);
+        }
+
+        static T[] newArray<T>(int size, Func<int, T> initialiser)
+        {
+            var result = new T[size];
+            for (int i = 0; i < size; i++)
+            {
+                result[i] = initialiser(i);
+            }
+            return result;
+        }
+    }
+}
--- Assets/PerplexingWiresModule.cs
+++ Assets/PerplexingWiresModule.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using PerplexingWires;
@@ -16,11 +17,215 @@ public class PerplexingWiresModule : MonoBehaviour
     public KMBombModule Module;
     public KMAudio Audio;
 
+    public Texture[] Arrows;
+    public Texture[] Stars;
+    public Material[] LedMaterials;
+
+    public MeshRenderer[] ArrowMeshes;
+    public MeshRenderer[] StarMeshes;
+    public MeshRenderer[] LedMeshes;
+
+    [Flags]
+    enum Arrow
+    {
+        Up = 0 << 0,
+        Left = 1 << 0,
+        Down = 2 << 0,
+        Right = 3 << 0,
+        DirectionMask = 3 << 0,
+
+        Red = 0 << 2,
+        Green = 1 << 2,
+        Blue = 2 << 2,
+        Yellow = 3 << 2,
+        Purple = 4 << 2,
+        ColorMask = 7 << 2
+    }
+
+    [Flags] enum Color { Red, Yellow, Blue, White, Green, Orange, Purple, Black }
+
+    private Arrow[] _arrows;
+    private bool[] _filledStars;
+    private bool[] _ledsOn;
+
+    enum CutRule { DontCut, CutFirst, CutLast, Cut }
+
+    sealed class WireInfo
+    {
+        public int TopConnector;
+        public int BottomConnector;
+        public Color Color;
+        public int Level;
+        public CutRule MustBeCut;
+        public bool HasBeenCut;
+    }
+    private WireInfo[] _wires = new WireInfo[6];
+
     private static int _moduleIdCounter = 1;
     private int _moduleId;
 
     void Start()
     {
         _moduleId = _moduleIdCounter++;
+        StartCoroutine(Initialize());
+    }
+
+    private IEnumerator Initialize()
+    {
+        yield return new WaitForSeconds(Rnd.Range(.1f, .5f));
+
+        retry:
+
+        //
+        // STEP 1: Decide on all the arrows, filled stars and LED states
+        //
+        _arrows = new Arrow[ArrowMeshes.Length];
+        for (int i = 0; i < ArrowMeshes.Length; i++)
+        {
+            _arrows[i] = (Arrow) Rnd.Range(0, (3 << 0) + (4 << 2) + 1);
+            ArrowMeshes[i].material.mainTexture = Arrows[(int) _arrows[i]];
+        }
+
+        _filledStars = new bool[StarMeshes.Length];
+        for (int i = 0; i < StarMeshes.Length; i++)
+        {
+            _filledStars[i] = Rnd.Range(0, 2) != 0;
+            StarMeshes[i].material.mainTexture = Stars[_filledStars[i] ? 1 : 0];
+        }
+
+        _ledsOn = new bool[LedMeshes.Length];
+        for (int i = 0; i < LedMeshes.Length; i++)
+        {
+            _ledsOn[i] = Rnd.Range(0, 2) != 0;
+            LedMeshes[i].material = LedMaterials[_ledsOn[i] ? 1 : 0];
+        }
+
+        //
+        // STEP 2: Assign the wires to their connectors and decide their colors
+        //
+        var shuffledBottom = Enumerable.Range(0, 6).ToList().Shuffle();
+        for (int i = 0; i < 6; i++)
+        {
+            _wires[i] = new WireInfo
+            {
+                TopConnector = i < 4 ? i : Rnd.Range(0, 4),
+                BottomConnector = shuffledBottom[i],
+                Color = (Color) Rnd.Range(0, 8),
+                Level = 1,
+                HasBeenCut = false
+            };
+            for (int j = 0; j < i; j++)
+                if ((_wires[j].TopConnector > _wires[i].TopConnector && _wires[j].BottomConnector < _wires[i].BottomConnector) ||
+                    (_wires[j].TopConnector < _wires[i].TopConnector && _wires[j].BottomConnector > _wires[i].BottomConnector))
+                    _wires[i].Level = Math.Max(_wires[i].Level, _wires[j].Level + 1);
+        }
+
+        //
+        // STEP 3: Determine the solution. Make sure that at least one wire needs to be cut.
+        //
+        const string rules = "LWIPMVIFIUCCFRHHTVUDLRJBQWBDJTQD";
+        var colorsForRedRule = new[] { Color.Red, Color.Yellow, Color.Blue, Color.White };
+        for (int i = 0; i < 6; i++)
+        {
+            var rule = 0;
+            // Blue: The wire crosses over another wire.
+            if (_wires.Any(w => (w.BottomConnector > _wires[i].BottomConnector && w.TopConnector < _wires[i].TopConnector) || (w.BottomConnector < _wires[i].BottomConnector && w.TopConnector > _wires[i].TopConnector)))
+                rule += 1;
+            // Yellow: The wire’s star is black.
+            if (_filledStars[_wires[i].TopConnector])
+                rule += 2;
+            // Green: The wire’s position on the bottom is even.
+            if (_wires[i].BottomConnector % 2 != 0)
+                rule += 4;
+            // Red: The wire is red, yellow, blue, or white.
+            if (colorsForRedRule.Contains(_wires[i].Color))
+                rule += 8;
+            // Orange: The wire shares the same color as its arrow.
+            if ((_wires[i].Color == Color.Red && (_arrows[_wires[i].BottomConnector] & Arrow.ColorMask) == Arrow.Red) ||
+                (_wires[i].Color == Color.Green && (_arrows[_wires[i].BottomConnector] & Arrow.ColorMask) == Arrow.Green) ||
+                (_wires[i].Color == Color.Blue && (_arrows[_wires[i].BottomConnector] & Arrow.ColorMask) == Arrow.Blue) ||
+                (_wires[i].Color == Color.Yellow && (_arrows[_wires[i].BottomConnector] & Arrow.ColorMask) == Arrow.Yellow) ||
+                (_wires[i].Color == Color.Purple && (_arrows[_wires[i].BottomConnector] & Arrow.ColorMask) == Arrow.Purple))
+                rule += 16;
+
+            var dir = (_arrows[_wires[i].BottomConnector] & Arrow.DirectionMask);
+            switch (rules[rule])
+            {
+                // C: Cut the wire.
+                case 'C': _wires[i].MustBeCut = CutRule.Cut; break;
+                // F: Always cut the wire, but only cut it first.
+                case 'F': _wires[i].MustBeCut = CutRule.CutFirst; break;
+                // L: Always cut the wire, but only cut it last.
+                case 'L': _wires[i].MustBeCut = CutRule.CutLast; break;
+                // W: Cut the wire if more of the LEDs are on than off.
+                case 'W': _wires[i].MustBeCut = _ledsOn.Count(l => l) > 1 ? CutRule.Cut : CutRule.DontCut; break;
+                // T: Cut the wire if the first LED is on.
+                case 'T': _wires[i].MustBeCut = _ledsOn[0] ? CutRule.Cut : CutRule.DontCut; break;
+                // U: Cut the wire if its arrow points up or down.
+                case 'U': _wires[i].MustBeCut = dir == Arrow.Up || dir == Arrow.Down ? CutRule.Cut : CutRule.DontCut; break;
+                // M: Cut the wire if the arrow points down or right.
+                case 'M': _wires[i].MustBeCut = dir == Arrow.Right || dir == Arrow.Down ? CutRule.Cut : CutRule.DontCut; break;
+                // H: Cut the wire if the wire shares a star with another wire.
+                case 'H': _wires[i].MustBeCut = _wires.Where((w, ix) => ix != i).Any(w => w.TopConnector == _wires[i].TopConnector) ? CutRule.Cut : CutRule.DontCut; break;
+                // P: Cut the wire if its position at the bottom is equal to the number of ports.
+                case 'P': _wires[i].MustBeCut = _wires[i].BottomConnector + 1 == Bomb.GetPortCount() ? CutRule.Cut : CutRule.DontCut; break;
+                // B: Cut the wire if its position at the bottom is equal to the number of batteries.
+                case 'B': _wires[i].MustBeCut = _wires[i].BottomConnector + 1 == Bomb.GetBatteryCount() ? CutRule.Cut : CutRule.DontCut; break;
+                // I: Cut the wire if its position at the bottom is equal to the number of indicators.
+                case 'I': _wires[i].MustBeCut = _wires[i].BottomConnector + 1 == Bomb.GetIndicators().Count() ? CutRule.Cut : CutRule.DontCut; break;
+                // Q: Cut the wire if the color of the wire is unique.
+                case 'Q': _wires[i].MustBeCut = _wires.Where((w, ix) => ix != i).Any(w => w.Color == _wires[i].Color) ? CutRule.DontCut : CutRule.Cut; break;
+                // J: Cut the wire if, at the bottom, it is adjacent to an orange or purple wire.
+                case 'J': _wires[i].MustBeCut = _wires.Any(w => (w.BottomConnector == _wires[i].BottomConnector - 1 || w.BottomConnector == _wires[i].BottomConnector + 1) && (w.Color == Color.Orange || w.Color == Color.Purple)) ? CutRule.Cut : CutRule.DontCut; break;
+                // V: Cut the wire if the serial number has a vowel, or if the bomb has a USB port.
+                case 'V': _wires[i].MustBeCut = Bomb.GetSerialNumberLetters().Any(ch => "AEIOU".Contains(ch)) || Bomb.GetPortCount("USB") > 0 ? CutRule.Cut : CutRule.DontCut; break;
+                // R: Cut the wire if its arrow direction is unique.
+                case 'R': _wires[i].MustBeCut = _arrows.Where((a, ix) => ix != _wires[i].BottomConnector).Any(a => (a & Arrow.DirectionMask) == dir) ? CutRule.DontCut : CutRule.Cut; break;
+                // D: Do not cut the wire.
+                case 'D': _wires[i].MustBeCut = CutRule.DontCut; break;
+            }
+        }
+        if (_wires.All(w => w.MustBeCut == CutRule.DontCut))
+            goto retry;
+
+        //
+        // STEP 4: Generate the actual wire meshes.
+        //
+        var wireOrig = Module.transform.FindChild("Wire").gameObject;
+        foreach (var wire in _wires)
+        {
+            var topConnector = Module.transform.FindChild("Strip2").FindChild("Connector" + (wire.TopConnector + 1));
+            var bottomConnector = Module.transform.FindChild("Strip1").FindChild("Connector" + (wire.BottomConnector + 1));
+            var seed = Rnd.Range(0, int.MaxValue);
+            var mesh = MeshGenerator.GenerateWire(
+                topConnector.position,
+                topConnector.FindChild("Control").position,
+                bottomConnector.FindChild("Control").position,
+                bottomConnector.position,
+                3,
+                MeshGenerator.WirePiece.Uncut,
+                highlight: false,
+                seed: seed,
+                raiseFactor: .75 * wire.Level);
+            var wireObj = Instantiate(wireOrig);
+            wireObj.GetComponent<MeshFilter>().mesh = mesh;
+            wireObj.transform.parent = Module.transform;
+
+            var highlightMesh = MeshGenerator.GenerateWire(
+                topConnector.position,
+                topConnector.FindChild("Control").position,
+                bottomConnector.FindChild("Control").position,
+                bottomConnector.position,
+                3,
+                MeshGenerator.WirePiece.Uncut,
+                highlight: true,
+                seed: seed,
+                raiseFactor: .75 * (wire.Level - 1));
+            var highlight = wireObj.transform.FindChild("Highlight");
+            //highlight.GetComponent<MeshFilter>().mesh = highlightMesh;
+
+            Debug.LogFormat("Wire ({0} to {1}) Level {3}: {2}", wire.TopConnector, wire.BottomConnector, wire.MustBeCut, wire.Level);
+        }
+        Destroy(wireOrig);
     }
 }
